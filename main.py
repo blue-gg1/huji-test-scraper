866f7337e12ad1d17b8571991ac01e016d7124a5
Steven Anthony Ballmer <thebigboss@clippers.com>
2026-08-24 13:01:53 +0300

    you got to start somehwere

---
+++ main.py
@@ -0,0 +1,21 @@
+
+
+
+curl 'https://www4.huji.ac.il/htbin/exams/exams.cgi' \
+  -X POST \
+  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:154.0) Gecko/20100101 Firefox/154.0' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' \
+  -H 'Accept-Language: en-US,en;q=0.9' \
+  -H 'Accept-Encoding: gzip, deflate, br, zstd' \
+  -H 'Referer: https://www4.huji.ac.il/htbin/exams/exams.cgi' \
+  -H 'Content-Type: application/x-www-form-urlencoded' \
+  -H 'Origin: https://www4.huji.ac.il' \
+  -H 'Connection: keep-alive' \
+  -H 'Cookie: glassix-visitor-id-v2-ae4edaa7-9020-46d9-8df3-114f0e6fec3c=9d669d7b-4c23-4c0d-9139-48ed91c6be25; TS016ac11e=01b025178ec1e04961ec26c47a2e5805a325b38aa3071eb788d5c4a208995fef56c7b6be369b200b155fb8044497a2abe192ee3dab; TS6368d62d027=082149a1b4ab2000d3cf882c57f91c9cf9e733b1dd0b30091d81ed29c62017648d6bb5adb3df2726084756c3b91130008f560bed0e525baa92753f2fdc7b2cfbde0348df7da96b04117a6b3b42022b9b527f31d2f13e04c3716005cae46d6bca' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'Sec-Fetch-Dest: document' \
+  -H 'Sec-Fetch-Mode: navigate' \
+  -H 'Sec-Fetch-Site: same-origin' \
+  -H 'Sec-Fetch-User: ?1' \
+  -H 'Priority: u=0, i' \
+  --data-raw 'action=mode2&coursenum=80131&coursename=&teachername=&keywords=&year2=2026&year1=2000&moed=0&semester=0&Submit=%F9%EC%E7'
